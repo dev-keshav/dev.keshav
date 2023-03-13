b9f7a71b87f44b1c4084b88a277e93498f706e09
dev-keshav <keshavkumar9431@gmail.com>
2023-03-13 11:30:04 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,168 @@
+const logotext = "KK";
+const meta = {
+    title: "Keshav Kumar",
+    description: "I’m Keshav Kumar _ Full stack devloper,currently working in a start-up",
+};
+
+const introdata = {
+    title: "I’m Keshav Kumar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "I'm a full-stack web developer with background in HTML5, CSS3, React, Javascript, Firebase, and Node.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a full stack web developer having experiences in HTML5, CSS3, Javascript, Reactjs, Nodejs and firebase.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "India",
+        date: "2023",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Node",
+        value: 60,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+];
+
+const services = [
+    // {
+    //     title: "UI & UX Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    {
+        title: "Web Apps",
+        description: "I'm developing and keeping up with the entire stack of web apps, including the front end, back end, and database administration. I am knowledgeable about programming languages, frameworks, and tools. I've worked with React, a programme used to build dynamic websites.",
+    },
+    {
+        title: "Wordpress Website",
+        description: "Both front-end and back-end development, along with the use of plugins and themes, are under my purview. My objective is to develop appealing, user-friendly websites that adhere to customer requirements.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "keshavkumar9431@gmail.com",
+    // YOUR_FONE: "After receiving your message",
+    // description: "Anytime you can message me, I will undoubtedly respond. Please send your question if you have any questions about technologies or any problems with software projects. ",
+    description: "Send your query ->",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
